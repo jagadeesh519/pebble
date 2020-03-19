809b35f1045f893b9c2a4e5226094092eb692619
Jagadeesh Kancherla <52498234+jagadeesh-kancherla@users.noreply.github.com>
2020-03-19 17:29:22 +0530

    final test

--- test.sh
+++ test.sh
@@ -3,5 +3,9 @@
 
 sudo docker image pull tomcat:8.0
 
+sudo docker container create --publish 8082:8080 --name pebble tomcat:8.0
 
+sudo docker image build -t jagadeesh/pebble ./
+
+sudo docker container run -it --publish 8081:8080 jagadeesh/pebble
 exit
